af9af6373951db5859be0f8550b1cf56bc0fa9f6
Dan Arnaiz <124132003+dan-arnaiz@users.noreply.github.com>
2023-04-05 13:51:47 +0000

    Significance 2

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ print("Testing git.")
 
 
 x = 0.062
+print(x)
 if x < 0.5:
-    print("There's a significant relationship betweent the two variables.")
+    print("There's a significant relationship between the two variables.")
 else:
     print("There's no significant relationship between the two variables.")
+
