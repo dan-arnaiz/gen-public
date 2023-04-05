8a030f9f69570cdb3d8bf48f6bd1a599b7d171a0
Dan Arnaiz <124132003+dan-arnaiz@users.noreply.github.com>
2023-04-05 08:47:08 -0500

    Significance

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ print("That works!")
 print("Synced successfully!")
 print("Extension installed.")
 print("Testing git.")
+
+
+x = 0.062
+if x < 0.5:
+    print("There's a significant relationship betweent the two variables.")
+else:
+    print("There's no significant relationship between the two variables.")
